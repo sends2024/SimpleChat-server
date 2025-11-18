866ed9bdf9823f9c36c934c30e6e188deeaaf0c0
swuY <swu341457@gmail.com>
2025-11-19 03:59:48 +0800

    feat: Init project

--- internal/pkg/ws/hub.go
+++ internal/pkg/ws/hub.go
@@ -74,7 +74,7 @@ func (hub *Hub) broadcast(channelID string, response *WSMessageResponse) {
 				return
 			}
 		} else {
-			fmt.Println("marshal error:", err)
+			fmt.Println("marshal errorrrrrrrrrrr:", err)
 		}
 	}
 }
